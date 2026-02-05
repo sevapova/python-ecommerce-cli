91e8892edebde3dc78dbd0b7b738efc3c9cedf94
sevapova <slatipova99@gmail.com>
2026-02-05 20:13:47 +0500

    models

--- app/models/order.py
+++ app/models/order.py
@@ -1,10 +1,10 @@
 class Order:
-    def __init__(self, id, user, items):
-        self.id = id
+    def __init__(self, user, items):
         self.user = user
         self.items = items
         self.total_price = 0
 
     def calculate_total(self):
+        self.total_price = 0
         for item in self.items:
             self.total_price += item.product.price * item.quantity
--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,8 @@
 class User:
-    def __init__(self, id, username, password, fist_name, last_name):
+    def __init__(self, id, username, password, first_name, last_name):
         self.id = id
         self.username = username
         self.password = password
-        self.fist_name = fist_name
+        self.first_name = first_name
         self.last_name = last_name
         self.cart = []
-        
