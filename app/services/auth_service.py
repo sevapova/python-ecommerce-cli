5b89ee5932bb7d1a4babe770ca22c8b091fed95a
sevapova <slatipova99@gmail.com>
2026-02-05 20:14:39 +0500

    services

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,20 +1,35 @@
-from app.models import User
-from app.utils.file_db import USERS
+from app.models.user import User
+from app.utils.file_db import load_db, save_db
+
 
 def register(username, password, first_name, last_name):
-    for i in USERS:
-        if i.username == username:
-            if i.username == username:
-                return None
-            
-    user = User(len(USERS) +1, username, password, first_name, last_name)
-    USERS.append(user)
-    return user
+    db = load_db()
+
+    for user in db["users"]:
+        if user["username"] == username:
+            print("Bunday username mavjud")
+            return None
+
+    new_user = {
+        "id": len(db["users"]) + 1,
+        "username": username,
+        "password": password,
+        "first_name": first_name,
+        "last_name": last_name
+    }
+
+    db["users"].append(new_user)
+    save_db(db)
+
+    return User(**new_user)
+
 
 def login(username, password):
-    for i in USERS:
-        if i in USERS:
-            if i.username == username and i.password == password:
-                return i
-    print("BUnday parol kiritib bo'lmaydi!")
+    db = load_db()
+
+    for user in db["users"]:
+        if user["username"] == username and user["password"] == password:
+            return User(**user)
+
+    print("Login yoki parol xato")
     return None
--- app/services/order_service.py
+++ app/services/order_service.py
@@ -1,10 +1,20 @@
 from app.models.order import Order
-from app.utils.file_db import ORDERS
+from app.utils.file_db import load_db, save_db
 
 
 def make_order(user):
+    db = load_db()
+
     order = Order(user, user.cart)
     order.calculate_total()
-    ORDERS.append(order)
+
+    db["orders"].append({
+        "id": len(db["orders"]) + 1,
+        "user_id": user.id,
+        "total_price": order.total_price
+    })
+
+    save_db(db)
     user.cart = []
+
     return order
