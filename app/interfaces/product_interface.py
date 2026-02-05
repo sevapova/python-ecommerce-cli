d05b0186009ec89f1e8fddefb500a7e8b469f7f0
sevapova <slatipova99@gmail.com>
2026-02-05 20:13:17 +0500

    interfaces

--- app/interfaces/product_interface.py
+++ app/interfaces/product_interface.py
@@ -1,14 +1,32 @@
 from app.models.product import Product
-from app.models.cart import CartItem
-from app.utils.file_db import PRODUCTS
+from app.utils.file_db import load_db, save_db
 
-def add_product():
-    PRODUCTS.append(Product(1, "TV", 1000, 10))
-    PRODUCTS.append(Product(2, "Laptop", 1500, 5))
-    PRODUCTS.append(Product(3, "Phone", 500, 20))
+
+def add_sample_products():
+    db = load_db()
+
+    if db["products"]:  
+        return
+
+    db["products"].append({
+        "id": 1,
+        "name": "Laptop",
+        "price": 1200,
+        "stock": 5
+    })
+    db["products"].append({
+        "id": 2,
+        "name": "Mouse",
+        "price": 20,
+        "stock": 50
+    })
+
+    save_db(db)
 
 
 def show_products():
-    print("\n------ Mahsulotlar -----")
-    for i in PRODUCTS:
-        print(f"{i.id}. {i.name} - ${i.price} (Stock: {i.stock})")
+    db = load_db()
+
+    print("\n--- Mahsulotlar ---")
+    for p in db["products"]:
+        print(f"{p['id']}. {p['name']} - {p['price']}$")
--- app/interfaces/user_interface.py
+++ app/interfaces/user_interface.py
@@ -1,25 +1,26 @@
 from app.services.auth_service import register, login
 
-def user_interface():
+
+def user_menu():
     print("\n1. Register")
     print("2. Login")
 
-    choice = input("Menyuni tanlang: ")
+    choice = input("Tanlang: ")
 
     if choice == "1":
         return register(
-            input("Username:"),
-            input("Password:"),
-            input("First Name:"), 
-            input("Last Name:")
+            input("Username: "),
+            input("Password: "),
+            input("First name: "),
+            input("Last name: ")
         )
-    
+
     elif choice == "2":
         return login(
-            input("Username:"),
-            input("Password:")
+            input("Username: "),
+            input("Password: ")
         )
-    
+
     else:
-        print("Bunday menyu yo'q!")
+        print("Bunday menu yo'q!")
         return None
